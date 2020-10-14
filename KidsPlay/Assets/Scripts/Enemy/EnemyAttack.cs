c2fae5471f60d667da0515be883a2fe3a47f0594
Sander Serrano Altena <s.serrano_altena@hotmail.com>
2020-10-14 21:26:20 +0200

    Added enemies + HUD+ player shooting+ player&enemy damage+ sound
    Need to add score, more enemies(spawner) & game over

--- KidsPlay/Assets/Scripts/Enemy/EnemyAttack.cs
+++ KidsPlay/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -3,23 +3,24 @@ using System.Collections;
 
 public class EnemyAttack : MonoBehaviour
 {
-    public float timeBetweenAttacks = 0.5f;
-    public int attackDamage = 10;
+    public float timeBetweenAttacks = 0.5f;// The time in seconds between each attack
+    public int attackDamage = 10;// The amount of health taken away per attack
 
 
     Animator anim;
     GameObject player;
     PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
-    bool playerInRange;
-    float timer;
+    EnemyHealth enemyHealth;
+    bool playerInRange;// Whether player is within the trigger collider and can be attacked
+    float timer;// Timer for counting up to the next attack
 
 
     void Awake ()
     {
+        // Setting up the references
         player = GameObject.FindGameObjectWithTag ("Player");
         playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent<EnemyHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
         anim = GetComponent <Animator> ();
     }
 
@@ -44,9 +45,10 @@ public class EnemyAttack : MonoBehaviour
 
     void Update ()
     {
+        // Add the time since Update was last called to the timer
         timer += Time.deltaTime;
 
-        if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
         {
             Attack ();
         }
--- KidsPlay/Assets/Scripts/Player/PlayerShooting.cs
+++ KidsPlay/Assets/Scripts/Player/PlayerShooting.cs
@@ -2,25 +2,28 @@
 
 public class PlayerShooting : MonoBehaviour
 {
-    public int damagePerShot = 20;
-    public float timeBetweenBullets = 0.15f;
-    public float range = 100f;
+    public int damagePerShot = 20;// The damage inflicted by each bullet
+    public float timeBetweenBullets = 0.15f;// The time between each shot
+    public float range = 100f;// The distance the gun can fire
 
 
-    float timer;
-    Ray shootRay = new Ray();
-    RaycastHit shootHit;
-    int shootableMask;
+    float timer;// A timer to determine when to fire
+    Ray shootRay = new Ray();// A ray from the gun end forwards
+    RaycastHit shootHit;// A raycast hit to get information about what was hit
+    int shootableMask;// A layer mask so the raycast only hits things on the shootable layer.
     ParticleSystem gunParticles;
     LineRenderer gunLine;
     AudioSource gunAudio;
     Light gunLight;
-    float effectsDisplayTime = 0.2f;
+    float effectsDisplayTime = 0.2f;// The proportion of the timeBetweenBullets that the effects will display for
 
 
     void Awake ()
     {
+        // Create a layer mask for the Shootable layer.
         shootableMask = LayerMask.GetMask ("Shootable");
+
+        // Set up the references
         gunParticles = GetComponent<ParticleSystem> ();
         gunLine = GetComponent <LineRenderer> ();
         gunAudio = GetComponent<AudioSource> ();
@@ -30,6 +33,7 @@ public class PlayerShooting : MonoBehaviour
 
     void Update ()
     {
+        // Add the time since Update was last called to the timer
         timer += Time.deltaTime;
 
 		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
@@ -62,23 +66,29 @@ public class PlayerShooting : MonoBehaviour
         gunParticles.Stop ();
         gunParticles.Play ();
 
+        // Enable the line renderer and set it's first position to be the end of the gun
         gunLine.enabled = true;
         gunLine.SetPosition (0, transform.position);
 
+        // Set the shootRay so that it starts at the end of the gun and points forward from the barrel
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
 
-        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+        // Perform the raycast against gameobjects on the shootable layer and if it hits something
+        if (Physics.Raycast (shootRay, out shootHit, range, shootableMask))
         {
+            // Try and find an EnemyHealth script on the gameobject hit
             EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
             if(enemyHealth != null)
             {
                 enemyHealth.TakeDamage (damagePerShot, shootHit.point);
             }
+            // Set the second position of the line renderer to the point the raycast hit
             gunLine.SetPosition (1, shootHit.point);
         }
         else
         {
+            // set the second position of the line renderer to the fullest extent of the gun's range
             gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
         }
     }
